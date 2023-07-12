c50e7e8ad6e99b3c38406677f359de39762a6ed4
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-12 23:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+
+	let rem = -1; 
+	let bi = "";
+
+	while(num > 0){
+	rem = num % 2;
+	num =Math.floor(num /2);
+	bi = rem+bi;	
+	}
+	
+  return bi;
 }
 
 window.decimalToBinary = decimalToBinary;
